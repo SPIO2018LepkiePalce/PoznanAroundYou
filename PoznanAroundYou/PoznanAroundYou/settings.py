ae2ac1adb8d2b9bcf3cd916490d4fda3eac8aab1
Mateusz Gładczak <mateusz@gladczak.com>
2019-06-01 10:15:18 +0200

    rem django_heroku

--- PoznanAroundYou/PoznanAroundYou/settings.py
+++ PoznanAroundYou/PoznanAroundYou/settings.py
@@ -123,4 +123,4 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
