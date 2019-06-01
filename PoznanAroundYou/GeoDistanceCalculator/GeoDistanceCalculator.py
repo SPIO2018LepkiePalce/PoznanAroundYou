7d842dcf7a2cf1102ecb20a117ed65364c2fd78e
StefMach <stefanmachura@gmail.com>
2019-06-01 13:13:38 +0200

    added unit tests

--- PoznanAroundYou/GeoDistanceCalculator/GeoDistanceCalculator.py
+++ PoznanAroundYou/GeoDistanceCalculator/GeoDistanceCalculator.py
@@ -1,4 +1,5 @@
 from haversine import haversine
+import unittest
 
 
 class GeoDistanceCalculator:
@@ -7,7 +8,19 @@ class GeoDistanceCalculator:
 
     @staticmethod
     def get_distance(user_location, target_location, dist_type):
-        distance = -1
         if dist_type == "greatcircle":
             distance = int(haversine(user_location, target_location, unit = 'm'))
+        else:
+            return -1
         return distance
+
+
+class TestGDC(unittest.TestCase):
+    def test_calculator_type(self):
+        self.assertEqual(GeoDistanceCalculator().get_distance((10, 10), (20, 20),""), -1)
+
+    def test_calculator_distance(self):
+        self.assertEqual(GeoDistanceCalculator().get_distance((10, 10), (20, 20), "greatcircle"), 1544759)
+
+if __name__ == '__main__':
+    unittest.main()
